0f3ee5c149fdd028a2457eb2e0597c1b9c4171e3
arunn123 <arunrcks143@gmail.com>
2020-01-24 09:27:42 +0530

    jenkins

---
+++ src/test/java/secondTest/secondTest.java
@@ -0,0 +1,31 @@
+package secondTest;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class secondTest {
+	
+	@Test
+	public void validateFirstTest() throws IOException {
+		WebDriver wd=new FirefoxDriver();
+		wd.manage().window().maximize();
+		wd.get("https://www.google.com");
+		WebElement data=wd.findElement(By.name("q"));
+		data.sendKeys("Chennai");
+		data.sendKeys(Keys.ENTER);
+		
+		File src=((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src, new File("C:\\Users\\Arun\\eclipse-workspace\\Github-test\\screenshot\\capture2.jpg"));
+	}
+
+}
